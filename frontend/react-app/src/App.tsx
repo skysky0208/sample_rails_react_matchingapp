f51fd306a052dc2a4ec9eb22dffbc206b4a3e08c
Natsumi Sakamoto <55139437+skysky0208@users.noreply.github.com>
2023-10-02 00:13:38 +0900

    docker setup

---
+++ frontend/react-app/src/App.tsx
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react'
+import { execTest } from 'lib/api/test'
+
+const App: React.FC = () => {
+  const [message, setMessage] = useState<String>("")
+
+  const handleExecTest = async() => {
+    const res = await execTest()
+
+    if (res.status === 200){
+      setMessage(res.data.message)
+    }
+  }
+
+  useEffect(() => {
+    handleExecTest()
+  }, [])
+
+  return (
+    <h1>{message}</h1>
+  )
+}
+
+export default App
