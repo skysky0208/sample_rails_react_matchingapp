65e2de67f11e254162a473ab204afd97af15d912
Natsumi Sakamoto <55139437+skysky0208@users.noreply.github.com>
2023-10-02 01:03:17 +0900

    deviseの導入

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+        include DeviseTokenAuth::Concerns::SetUserByToken
+
+        # tokenによるCSRF対策を放棄
+        skip_before_action :verify_authenticity_token
+
+        helper_method :current_user, :user_signed_in?
 end
