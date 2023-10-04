d68716e373392e2869b5e4db016376d67d0e9f02
Natsumi Sakamoto <55139437+skysky0208@users.noreply.github.com>
2023-10-05 03:06:45 +0900

    frontend/インターフェースの定義

--- frontend/react-app/src/interfaces/index.ts
+++ frontend/react-app/src/interfaces/index.ts
@@ -3,6 +3,14 @@ export interface SignUpData {
     email: string
     password: string
     passwordConfirmation: string
+    gender: number
+    prefecture: number
+    birthday: Date
+    image: string
+}
+
+export interface SignUpFormData extends FormData {
+    append(name: keyof SignUpData, value: String | Blob, fileName?: string): any
 }
 
 export interface SignInData {
@@ -16,7 +24,47 @@ export interface User {
     provider: string
     email: string
     name: string
-    nickname?: string
-    image?: string
+    image: {
+        url: string
+    }
+    gender: number
+    birthday: String | number | Date
+    profile: string
+    prefecture: number
     allowPasswordChange: boolean
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+export interface UpdateUserData {
+    id: number | undefined | null
+    name?: string
+    prefecture?: number
+    prolfile?: string
+    image?: string
+}
+
+export interface UpdateUserFormData extends FormData {
+    append(name: keyof UpdateUserData, value: String | Blob, fileName?: string): any
+}
+
+export interface Like {
+    id?: number
+    fromUserId: number | undefined | null
+    toUserId: number | undefined | null
+}
+
+export interface ChatRoom {
+    chatRoom: {
+        id: number
+    }
+    otherUser: User
+    lastMessage: Message
+}
+
+export interface Message {
+    chatRoomId: number
+    userId: number | undefined
+    content: string
+    createdAt?: Date
 }
