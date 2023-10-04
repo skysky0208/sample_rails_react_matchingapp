f4bf3f5c8d5d159742a11c2b1b64bf324ac29621
Natsumi Sakamoto <55139437+skysky0208@users.noreply.github.com>
2023-10-04 23:40:56 +0900

    backend/ルーティング設定

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,14 +2,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :test, only: [:index]
+      resources :test, only: %i[index]
+      resources :likes, only: %i[index, create]
+      resources :chat_rooms, only: %i[index, show]
+      resources :messages, only: %i[create]
+      resources :users, only: %i[index, create, update]
 
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations'
       }
 
       namespace :auth do
-        resources :sessions, only: [:index]
+        resources :sessions, only: %i[index]
       end
     end
   end
