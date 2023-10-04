9649f2576e9fb436ff6dee0119ebf6aefd665fb0
Natsumi Sakamoto <55139437+skysky0208@users.noreply.github.com>
2023-10-04 22:28:19 +0900

    backend/Chat機能

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -15,4 +15,7 @@ class User < ActiveRecord::Base
   has_many :active_likes, through: :likes_from, source: :to_user
   has_many :passive_likes, through: :likes_to, source: :from_user
 
+  has_many :chat_room_users
+  has_many :chat_rooms, through :chat_room_users
+
 end
