795241f9f2f79d166e7ef789e88a57eabfc69179
Natsumi Sakamoto <55139437+skysky0208@users.noreply.github.com>
2023-10-04 22:17:02 +0900

    backend/Like機能

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -8,4 +8,11 @@ class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
 
   mount_uploader :image, ImageUploader
+
+  has_many :likes_from, class_name: "Like", foreign_key: :from_user_id, dependent: :destory
+  has_many :likes_to, class_name: "Like", foreign_key: :to_user_id, dependent: :destory
+
+  has_many :active_likes, through: :likes_from, source: :to_user
+  has_many :passive_likes, through: :likes_to, source: :from_user
+
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_04_014346) do
+ActiveRecord::Schema.define(version: 2023_10_04_130413) do
+
+  create_table "likes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "from_user_id", null: false
+    t.integer "to_user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
